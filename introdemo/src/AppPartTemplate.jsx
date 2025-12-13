38616781f4d4cee2e0384d284d6df0e649fa4d17
Hugo Nakamura <hugognakamura@gmail.com>
2025-12-13 17:19:28 -0300

    uibuild

--- introdemo/src/AppPartTemplate.jsx
+++ introdemo/src/AppPartTemplate.jsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <div>
             <h1>PartTemplate</h1>
-            <p> shx is working </p>
+            <p> shx is working 2 </p>
         </div>
     )
 }
