fca4aeffa6516f1b15a587d4d0fe8344babd19e9
Hugo Nakamura <hugognakamura@gmail.com>
2025-12-15 14:52:12 -0300

    uibuild

--- part3-node-express/index.js
+++ part3-node-express/index.js
@@ -86,7 +86,7 @@ app.post('/api/notes', (request, response) => {
 
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
