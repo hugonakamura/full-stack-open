abee4067fdbc968dd41e262696703c82bce2bad4
Hugo Nakamura <hugognakamura@gmail.com>
2025-12-18 16:44:25 -0300

    refactor: app.post using async/await

--- part3-node-express/controllers/notes.js
+++ part3-node-express/controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -39,7 +38,7 @@ notesRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -53,11 +52,8 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important ?? false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 
