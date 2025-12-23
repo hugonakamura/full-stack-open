7609583f1f7bdc7786bcb5806a90b7220d334729
Hugo Nakamura <hugognakamura@gmail.com>
2025-12-23 15:36:07 -0300

    fix: userSchema

--- part3-node-express/models/user.js
+++ part3-node-express/models/user.js
@@ -5,8 +5,10 @@ const userSchema = new mongoose.Schema({
   name: String,
   passwordHash: String,
   notes: [
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Note'
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
   ],
 })
 
