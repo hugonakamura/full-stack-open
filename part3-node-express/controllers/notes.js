b855dbd148cde1070815aaffd8fce857561e4887
Hugo Nakamura <hugognakamura@gmail.com>
2025-12-26 16:45:26 -0300

    feat: add token auth

--- part3-node-express/controllers/notes.js
+++ part3-node-express/controllers/notes.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
@@ -38,10 +40,22 @@ notesRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
@@ -60,7 +74,6 @@ notesRouter.post('/', async (request, response) => {
   })
 
   const savedNote = await note.save()
-
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
 
