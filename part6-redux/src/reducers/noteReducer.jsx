e1dc73a3639a09257135f5562c8c6fe20d0b6adb
Hugo Nakamura <hugognakamura@gmail.com>
2026-01-16 11:17:02 -0300

    feat: finish part 6b

--- part6-redux/src/reducers/noteReducer.jsx
+++ part6-redux/src/reducers/noteReducer.jsx
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, current } from '@reduxjs/toolkit'
+
 
 const initialState = [
   {
@@ -35,6 +36,9 @@ const noteSlice = createSlice({
         ...noteToChange,
         important: !noteToChange.important
       }
+
+      console.log(current(state))
+
       return state.map(note =>
         note.id !== id ? note : changedNote
       )
